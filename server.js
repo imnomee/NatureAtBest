cf19d95e96a01387fcf7804967e2d1e25d38dc47
imnomee <59981447+imnomee@users.noreply.github.com>
2020-03-31 09:07:51 +0100

    ignore updated

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL //local database connections
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log('Connected to the databases'))
+  .catch((err) => console.error(err));
 /*
 environment variables available at
 >> console.log(process.env);
